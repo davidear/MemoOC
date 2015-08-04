de0017ddf0bed655bf459b9eef17871d5bbb9e86
dai.fengyi <fengji@zkjinshi.com>
2015-08-04 11:06:30 +0800

    Add a prompt for fingerprint switch button

--- MemoOC/Module/Setting/MMSettingTableViewController.m
+++ MemoOC/Module/Setting/MMSettingTableViewController.m
@@ -49,6 +49,9 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
     [super viewDidLoad];
     [self setUI];
 }
+- (void)viewWillAppear:(BOOL)animated {//可以在三种情况的presented的vc被dismiss的时候进行动作，不过使代码太难读，这里牺牲一些性能
+    [self.tableView reloadData];
+}
 
 - (void)loadData {
     NSString *path = [[NSBundle mainBundle] pathForResource:@"SettingList" ofType:@"plist"];
@@ -56,6 +59,11 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
 }
 - (void)setUI {
     [self.tableView registerClass:[MMSettingSwitchCell class] forCellReuseIdentifier:SwitchReuseIdentifier];
+    UILabel *header = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, 21)];
+    header.text = @"指纹识别仅在已设置密码情况下才能开启";
+    header.textAlignment = NSTextAlignmentCenter;
+    header.font = [UIFont systemFontOfSize:13];
+    self.tableView.tableHeaderView = header;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -87,14 +95,18 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
         cell = [tableView dequeueReusableCellWithIdentifier:SwitchReuseIdentifier forIndexPath:indexPath];
         UISwitch *switchButton = (UISwitch *)cell.accessoryView;
         [switchButton addTarget:self action:@selector(switchValueChanged:) forControlEvents:UIControlEventValueChanged];
+        switchButton.enabled = [[VENTouchLock sharedInstance] isPasscodeSet] && [VENTouchLock canUseTouchID];
+        switchButton.on = [VENTouchLock shouldUseTouchID];
     }
     // Configure the cell...
     cell.textLabel.text = self.dataArray[indexPath.row][@"title"];
     return cell;
 }
+
+
 #pragma mark - Button Action
 - (void)switchValueChanged:(UISwitch *)sender {
-    
+    [VENTouchLock setShouldUseTouchID:sender.isOn];
 }
 #pragma mark - Table View Delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -114,7 +126,6 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
             
         }else {//未设置密码
             VENTouchLockCreatePasscodeViewController *vc = [[VENTouchLockCreatePasscodeViewController alloc] init];
-            //        [self.navigationController pushViewController:vc animated:YES];
             MMNavigationController *nvc = [[MMNavigationController alloc] initWithRootViewController:vc];
             [self presentViewController:nvc animated:YES completion:nil];
         }
@@ -132,8 +143,6 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
             [self presentViewController:nvc animated:YES completion:nil];
         }
     }
-
-
 }
 
 - (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
