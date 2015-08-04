e7a9ad1ad50871ca71889610bb095fad18faf2b3
dai.fengyi <fengji@zkjinshi.com>
2015-08-04 11:21:18 +0800

    change the button text depending ispasscodeset and trim the code

--- MemoOC/Module/Setting/MMSettingTableViewController.m
+++ MemoOC/Module/Setting/MMSettingTableViewController.m
@@ -21,8 +21,8 @@
     }
     return self;
 }
-
 @end
+#pragma mark -
 @interface MMSettingTableViewController ()
 @property (strong, nonatomic) NSMutableArray *dataArray;
 @end
@@ -39,12 +39,6 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
     return self;
 }
 
-//- (void)awakeFromNib {
-//    UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithTitle:@"关闭" style:UIBarButtonItemStylePlain target:self action:@selector(dismissVC)];
-//    [left setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor colorFromHexString:kColorDark]} forState:UIControlStateNormal];
-//    self.navigationItem.leftBarButtonItem = left;
-//
-//}
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setUI];
@@ -66,16 +60,7 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
     self.tableView.tableHeaderView = header;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-//#pragma mark - Button action
-//- (void)dismissVC {
-//    [self dismissViewControllerAnimated:YES completion:nil];
-//}
 #pragma mark - Table view data source
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
     return 1;
@@ -85,7 +70,6 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
     // Return the number of rows in the section.
     return self.dataArray.count;
 }
-
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell;
@@ -100,9 +84,11 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
     }
     // Configure the cell...
     cell.textLabel.text = self.dataArray[indexPath.row][@"title"];
+    if (indexPath.row == 0 && [[VENTouchLock sharedInstance] isPasscodeSet]) {
+        cell.textLabel.text = self.dataArray[indexPath.row][@"title2"];
+    }
     return cell;
 }
-
 
 #pragma mark - Button Action
 - (void)switchValueChanged:(UISwitch *)sender {
@@ -151,48 +137,4 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
     }
     return YES;
 }
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
