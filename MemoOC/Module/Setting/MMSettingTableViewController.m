f5fadc0a881dd227c068cb72ea10755cbfb2942a
dai.fengyi <fengji@zkjinshi.com>
2015-08-04 10:40:09 +0800

    Add delete passcode entrance

--- MemoOC/Module/Setting/MMSettingTableViewController.m
+++ MemoOC/Module/Setting/MMSettingTableViewController.m
@@ -81,15 +81,13 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell;
-    if (indexPath.row == 0) {
+    if (indexPath.row < 2) {
         cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
-    }else {
+    }else if(indexPath.row == 2) {
         cell = [tableView dequeueReusableCellWithIdentifier:SwitchReuseIdentifier forIndexPath:indexPath];
         UISwitch *switchButton = (UISwitch *)cell.accessoryView;
         [switchButton addTarget:self action:@selector(switchValueChanged:) forControlEvents:UIControlEventValueChanged];
     }
-    
-    
     // Configure the cell...
     cell.textLabel.text = self.dataArray[indexPath.row][@"title"];
     return cell;
@@ -101,29 +99,45 @@ static NSString * const SwitchReuseIdentifier = @"SwitchCell";
 #pragma mark - Table View Delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if ([[VENTouchLock sharedInstance] isPasscodeSet]) {
-        VENTouchLockEnterPasscodeViewController *vc = [[VENTouchLockEnterPasscodeViewController alloc] init];
-        __weak typeof(vc) weakself = vc;
-        vc.willFinishWithResult = ^void(BOOL success) {
-            if (success) {
-                VENTouchLockCreatePasscodeViewController *createVC = [[VENTouchLockCreatePasscodeViewController alloc] init];
-                [weakself.navigationController pushViewController:createVC animated:YES];
-            }
-        };
-        MMNavigationController *nvc = [[MMNavigationController alloc] initWithRootViewController:vc];
-        [self presentViewController:nvc animated:YES completion:nil];
-
-    }else {
-        VENTouchLockCreatePasscodeViewController *vc = [[VENTouchLockCreatePasscodeViewController alloc] init];
-//        [self.navigationController pushViewController:vc animated:YES];
-        MMNavigationController *nvc = [[MMNavigationController alloc] initWithRootViewController:vc];
-        [self presentViewController:nvc animated:YES completion:nil];
+    if (indexPath.row == 0) {
+        if ([[VENTouchLock sharedInstance] isPasscodeSet]) {//已设置密码
+            VENTouchLockEnterPasscodeViewController *vc = [[VENTouchLockEnterPasscodeViewController alloc] init];
+            __weak typeof(vc) weakself = vc;
+            vc.willFinishWithResult = ^void(BOOL success) {
+                if (success) {
+                    VENTouchLockCreatePasscodeViewController *createVC = [[VENTouchLockCreatePasscodeViewController alloc] init];
+                    [weakself.navigationController pushViewController:createVC animated:YES];
+                }
+            };
+            MMNavigationController *nvc = [[MMNavigationController alloc] initWithRootViewController:vc];
+            [self presentViewController:nvc animated:YES completion:nil];
+            
+        }else {//未设置密码
+            VENTouchLockCreatePasscodeViewController *vc = [[VENTouchLockCreatePasscodeViewController alloc] init];
+            //        [self.navigationController pushViewController:vc animated:YES];
+            MMNavigationController *nvc = [[MMNavigationController alloc] initWithRootViewController:vc];
+            [self presentViewController:nvc animated:YES completion:nil];
+        }
+    }else if(indexPath.row == 1) {
+        if ([[VENTouchLock sharedInstance] isPasscodeSet]) {
+            VENTouchLockEnterPasscodeViewController *vc = [[VENTouchLockEnterPasscodeViewController alloc] init];
+            __weak typeof(vc) weakself = vc;
+            vc.willFinishWithResult = ^void(BOOL success) {
+                if (success) {
+                    [[VENTouchLock sharedInstance] deletePasscode];
+                    [weakself dismissViewControllerAnimated:YES completion:nil];
+                }
+            };
+            MMNavigationController *nvc = [[MMNavigationController alloc] initWithRootViewController:vc];
+            [self presentViewController:nvc animated:YES completion:nil];
+        }
     }
+
 
 }
 
 - (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.row == 1) {
+    if (indexPath.row == 2) {
         return NO;
     }
     return YES;
